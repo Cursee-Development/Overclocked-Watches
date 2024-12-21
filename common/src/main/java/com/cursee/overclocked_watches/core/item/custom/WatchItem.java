800c80659ddf18a457621055034fcb05a16c2767
jason13official <jason13official@gmail.com>
2024-12-21 11:49:20 -0500

    Create WatchItem.java

---
+++ common/src/main/java/com/cursee/overclocked_watches/core/item/custom/WatchItem.java
@@ -0,0 +1,10 @@
+package com.cursee.overclocked_watches.core.item.custom;
+
+import net.minecraft.world.item.Item;
+
+public class WatchItem extends Item {
+
+    public WatchItem(Properties properties) {
+        super(properties);
+    }
+}
