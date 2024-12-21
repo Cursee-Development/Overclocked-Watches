7fcc78591584e9af7f1408c0ab99f439466c1cea
jason13official <jason13official@gmail.com>
2024-12-21 11:59:11 -0500

    implement client entrypoints

--- forge/src/main/java/com/cursee/overclocked_watches/OverclockedWatchesForge.java
+++ forge/src/main/java/com/cursee/overclocked_watches/OverclockedWatchesForge.java
@@ -1,9 +1,13 @@
 package com.cursee.overclocked_watches;
 
 import com.cursee.monolib.core.sailing.Sailing;
+import com.cursee.overclocked_watches.client.OverclockedWatchesClientForge;
 import com.cursee.overclocked_watches.core.registry.RegistryForge;
+import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import net.minecraftforge.fml.loading.FMLEnvironment;
 
 @Mod(Constants.MOD_ID)
 public class OverclockedWatchesForge {
@@ -11,6 +15,10 @@ public class OverclockedWatchesForge {
     public OverclockedWatchesForge() {
         OverclockedWatches.init();
         Sailing.register(Constants.MOD_NAME, Constants.MOD_ID, Constants.MOD_VERSION, Constants.MC_VERSION_RAW, Constants.PUBLISHER_AUTHOR, Constants.PRIMARY_CURSEFORGE_MODRINTH);
-        RegistryForge.register(FMLJavaModLoadingContext.get().getModEventBus());
+
+        IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
+
+        RegistryForge.register(modEventBus);
+        if (FMLEnvironment.dist == Dist.CLIENT) new OverclockedWatchesClientForge(modEventBus);
     }
 }
---
+++ forge/src/main/java/com/cursee/overclocked_watches/client/OverclockedWatchesClientForge.java
@@ -0,0 +1,9 @@
+package com.cursee.overclocked_watches.client;
+
+import net.minecraftforge.eventbus.api.IEventBus;
+
+public class OverclockedWatchesClientForge {
+
+    public OverclockedWatchesClientForge(IEventBus modEventBus) {
+    }
+}
